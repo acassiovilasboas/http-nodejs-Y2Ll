902b3d6e82e771d9ae87bc9b69ea6f0c76e6e55e
Acassio Mendonça Vilas Boas <47704536+acassiovilasboas@users.noreply.github.com>
2022-10-04 21:51:22 -0300

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,6 +1,6 @@
 import { createServer } from 'http';
 
 createServer((req, res) => {
-  res.json({"message":"Minha primeira API"});
+  res.json({"message": "Minha primeira API"});
   res.end();
 }).listen(process.env.PORT);
